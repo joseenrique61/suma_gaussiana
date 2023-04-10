8e570211eef86fa98a156db0e7b3497c73a566bc
Laboratorio <laboratorio@udla-ec.int>
2023-04-10 15:27:34 -0500

    First commit

---
+++ suma_gauss.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int suma_gauss();
+int suma_repetitiva();
+
+int main() 
+{
+    int num;
+    printf("Ingrese un numero:");
+    scanf("%i", &num);
+    
+    printf("Suma Gaussiana: %i\n", suma_gauss(num));
+    printf("Suma repetitiva: %i\n", suma_repetitiva(num));
+}
+
+int suma_gauss(int num) 
+{
+    int result = (num * (num + 1))/2;
+    return result;
+}
+
+int suma_repetitiva(int num)
+{
+    int result = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        result += i;
+    }
+    return result;
+}
